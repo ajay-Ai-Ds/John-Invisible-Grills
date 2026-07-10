032071d7a0792043591a74d4219f7b5c006f2334
Ajay <ajay@example.com>
2026-07-11 00:33:45 +0530

    Add Tawk.to live chat widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,6 +89,20 @@ export default function RootLayout({
         <FloatingButtons />
 
 
+        {/* Live Chat - Tawk.to */}
+        <Script id="tawk-to" strategy="lazyOnload">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6a5141b44c9e9d1d45c93c85/1jt6mhb7q';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
